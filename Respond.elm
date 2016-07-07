d97b81d82322e71ab95467d6f1e4344d78066917
Isaac Shapira <fresheyeball@gmail.com>
2016-07-06 23:42:16 -0600

    Respond migrated

---
+++ Respond.elm
@@ -0,0 +1,44 @@
+module Respond exposing (..)
+
+{-|
+@docs Respond, append, sum, zero, comap
+-}
+
+
+{-|
+A function from a model to a Cmd.
+Basically there are times where you want to
+have a side effect on the world if the model
+has a certain shape. `Respond` facilitates
+this use case.
+-}
+type alias Respond msg a =
+    a -> Cmd msg
+
+
+{-| -}
+append : Respond msg a -> Respond msg a -> Respond msg a
+append f g a =
+    Cmd.batch [ f a, g a ]
+
+
+{-| -}
+sum : List (Respond msg a) -> Respond msg a
+sum rs a =
+    List.map (\r -> r a) rs
+        |> Cmd.batch
+
+
+{-| -}
+zero : Respond msg a
+zero =
+    always Cmd.none
+
+
+{-|
+Add a function to the front
+`b -> a >> a -> Cmd msg`
+-}
+comap : (b -> a) -> Respond msg a -> Respond msg b
+comap f fa =
+    fa << f
